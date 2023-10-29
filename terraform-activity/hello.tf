2b8d80cba8a7d93f09ef1ffc6ce03cbc8397109c
Rahul <rahul.ranjan7201@gmail.com>
2023-10-29 11:21:34 +0000

    Added Subscription

--- terraform-activity/hello.tf
+++ terraform-activity/hello.tf
@@ -11,4 +11,7 @@ terraform {
 provider "azurerm" {
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
+  subscription_id = "278980ef-cfef-4a86-b0e6-0bc9e1286a41"
+  tenant_id = "199ee0d2-f16c-4bcc-8282-a04937c02e5d"
+
 }
